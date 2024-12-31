3aff0e9709de9e3fdb01eb47699e82381187b316
douglasmadeiro1 <135288383+douglasmadeiro1@users.noreply.github.com>
2024-12-31 09:44:58 -0300

    register function
    register function

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -11,13 +11,59 @@ loginBtn.addEventListener('click', () => {
 });
 
 function login() {
-    const email = document.getElementById('emailLogin').value;
-    const password = document.getElementById('passwordLogin').value;
+    const email = form.emailLogin().value;
+    const password = form.passwordLogin().value;
 
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then(response => {
             window.location.href = "assets/pages/dashboard.html";
         }).catch(error => {
-            console.log('error', error);
+            console.log(getErrorMessage(error));
         });
+}
+
+function register() {
+
+    const email = form.emailRegister().value;
+    const password = form.passwordRegister().value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        window.location.href = "assets/pages/dashboard.html";
+    }).catch(error => {
+        console.log(getErrorMessage(error));
+        });
+}
+
+function recoveryPassword() {
+    firebase.auth().sendPasswordResetEmail(form.emailLogin().value).then(() => {
+        alert('Email de recuperação enviado com sucesso');
+    }).catch(error => {
+        alert('Erro ao enviar email de recuperação, verifique o email informado');
+    });
+}
+
+const form = {
+    emailLogin: () => document.getElementById('emailLogin'),
+    passwordLogin: () => document.getElementById('passwordLogin'),
+    emailRegister: () => document.getElementById('emailRegister'),
+    passwordRegister: () => document.getElementById('passwordRegister'),
+}
+
+function getErrorMessage(error) {
+    if (!error) {
+        return "Ocorreu um erro desconhecido.";
+    }
+
+    // Verificando erros conhecidos do Firebase
+    if (error.code === "auth/user-not-found") {
+        return "Usuário não encontrado.";
+    }
+    if (error.code === "auth/wrong-password") {
+        return "Senha inválida.";
+    }
+    if (error.code === "auth/invalid-credential") {
+        return "Credenciais inválidas. Verifique seu e-mail e senha.";
+    }
+
+    // Para outros erros não especificados
+    return error.message || "Erro desconhecido.";
 }
